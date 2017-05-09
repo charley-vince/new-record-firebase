d0281193005d0b3d14bd0e595702a317327e5878
Kaign <ignatkond@gmail.com>
2017-05-10 01:07:25 +0300

    fixed bug with hot replacement

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+import {AppContainer} from 'react-hot-loader'
 import App from './components/App.js'
+import createStoreForPage from './store'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(
-  <AppContainer>
-    <App/>
-  </AppContainer>,
-  document.getElementById('root')
-);
+let store = createStoreForPage()
+const render = App => {
+	ReactDOM.render(
+		<AppContainer>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</AppContainer>,
+		document.getElementById('root')
+	)
+}
+
+render(App)
 
 // Hot Module Replacement API
-
 if (module.hot) {
-  module.hot.accept('./components/App', () => { render(App) })
+	module.hot.accept('./components/App.js', () => {
+		render(App)
+	})
 }
