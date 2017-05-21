301ad7f8c9e7f11b12c428a6080a30b91743c634
Kaign <ignatkond@gmail.com>
2017-05-21 21:52:09 +0300

    postcss autoprefixier and image compression

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,48 +1,79 @@
-let path = require('path')
-let webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
 console.log(__dirname)
 // var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
-	devtool: 'eval',
-	devServer: {
-		headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*'}
-	},
-	entry: ['react-hot-loader/patch', 'webpack-hot-middleware/client', './src/index'],
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/'
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-		// new BundleAnalyzerPlugin()
-	],
-	resolve: {
-		alias: {
-			Styles: path.resolve(__dirname, 'public/styles/'),
-			Images: path.resolve(__dirname, 'public/img/')
-		}
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loaders: ['babel-loader'],
-				include: path.join(__dirname, 'src')
-			},
-			{
-				test: /\.less$/,
-				loader: 'style-loader!css-loader!less-loader'
-			},
-			{
-				test: /\.(jpg|png|svg|gif)$/,
-				loader: 'file-loader?name=assets/img/[name].[ext]'
-			},
-			{
-				test: /\.(ttf|otf)$/,
-				loader: 'file-loader?name=assets/fonts/[name].[ext]'
-			}
-		]
-	}
+  devtool: 'eval',
+  devServer: {
+    headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*'}
+  },
+  entry: ['react-hot-loader/patch', 'webpack-hot-middleware/client', './src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+    // new BundleAnalyzerPlugin()
+  ],
+  resolve: {
+    alias: {
+      Styles: path.resolve(__dirname, 'public/styles/'),
+      Images: path.resolve(__dirname, 'public/img/')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [autoprefixer]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif)$/,
+        use: [
+          'file?name=assets/img/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true
+                },
+                gifsicle: {
+                  interlaced: true
+                },
+                optipng: {
+                  optimizationLevel: 7
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|otf)$/,
+        loader: 'file-loader?name=assets/fonts/[name].[ext]'
+      }
+    ]
+  }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,58 +1,89 @@
 let path = require('path')
 let webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
 // let CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
-	devtool: 'cheap-module-source-map',
-	entry: './src/index',
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/'
-	},
-	resolve: {
-		alias: {
-			Styles: path.resolve(__dirname, 'public/styles/'),
-			Images: path.resolve(__dirname, 'public/img/')
-		}
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loaders: ['babel-loader'],
-				include: path.join(__dirname, 'src')
-			},
-			{
-				test: /\.less$/,
-				loader: ['style-loader', 'css-loader', 'less-loader']
-			},
-			{
-				test: /\.(jpg|png|svg|gif)$/,
-				loader: 'file-loader?name=assets/img/[name].[ext]'
-			},
-			{
-				test: /\.(ttf|otf)$/,
-				loader: 'file-loader?name=assets/fonts/[name].[ext]'
-			}
-		]
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production')
-			}
-		}),
-		// new CompressionPlugin({
-		// 	asset: '[path].gz[query]',
-		// 	algorithm: 'gzip',
-		// 	test: /\.(js|html)$/,
-		// 	threshold: 10240,
-		// 	minRatio: 0.8
-		// }),
-		// new webpack.IgnorePlugin(/redux-devtools-extension/),
-		// new webpack.IgnorePlugin(/redux-logger/),
-		new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
-		new webpack.optimize.AggressiveMergingPlugin()
-	]
+  devtool: 'cheap-module-source-map',
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    alias: {
+      Styles: path.resolve(__dirname, 'public/styles/'),
+      Images: path.resolve(__dirname, 'public/img/')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [autoprefixer]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif)$/,
+        use: [
+          'file-loader?name=assets/img/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true
+                },
+                gifsicle: {
+                  interlaced: true
+                },
+                optipng: {
+                  optimizationLevel: 7
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|otf)$/,
+        loader: 'file-loader?name=assets/fonts/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    // new CompressionPlugin({
+    // 	asset: '[path].gz[query]',
+    // 	algorithm: 'gzip',
+    // 	test: /\.(js|html)$/,
+    // 	threshold: 10240,
+    // 	minRatio: 0.8
+    // }),
+    // new webpack.IgnorePlugin(/redux-devtools-extension/),
+    // new webpack.IgnorePlugin(/redux-logger/),
+    new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
 }
